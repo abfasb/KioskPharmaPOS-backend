781d99ee93214fc56f7cb10c55a789be7fe263ae
mtthw <matbalinton@gmail.com>
2024-10-27 14:39:41 +0800

    Add firebase-admin sdk

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,8 @@
 const admin = require('firebase-admin');
 const dotenv = require('dotenv');
+
+
 dotenv.config();
-console.log(process.env.VITE_APP_FIREBASE_API_KEY);
 
 admin.initializeApp({
     credential: admin.credential.applicationDefault(), 
